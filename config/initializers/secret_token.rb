69f853d607c509f8f18018d8b2885a28ae3b7651
Nick Evans <me@nicolasevans.org>
2013-09-01 00:56:25 -0400

    create rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NHLTracker::Application.config.secret_token = 'd10d55e0e5c0faa3ed89c1158df5a5b76564b4cff7859432b5512ac4d51eaf90c97ef37096487a1cbc241772671e8cbf7056edabb154fabb6f075b5b95878b4a'
