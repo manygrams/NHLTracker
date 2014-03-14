372f88a3abd893954835e2b61cdb0dfbd4009330
Nick Evans <me@nicolasevans.org>
2014-03-14 14:07:12 -0400

    typo

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   attr_accessible :away_player_id, :away_team, :away_player_score, :home_player_id, :home_player_score, :home_team, :archived
 
-  before_validate :set_archived_as_false
+  before_validation :set_archived_as_false
 
   scope :active, -> { where(archived: false) }
 
