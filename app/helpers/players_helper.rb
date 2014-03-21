806462cbe899c6c74436c8b01f1aed0eaa41f933
Nick Evans <me@nicolasevans.org>
2014-03-21 03:12:25 -0400

    add in jquery fade for players with no wins

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,9 @@
 module PlayersHelper
+  def ranked_list_class(player)
+    player.games_won.count == 0 ? 'no-wins' : 'has-won'
+  end
+
+  def players_with_no_wins?
+    @ranked_players.any? { |player| player.games_won.count == 0 }
+  end
 end
